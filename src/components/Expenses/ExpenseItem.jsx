f4cfe6877f7140d7ad2a1161ac85051e64861ced
Tipchan Songtheng <tipchansongtheng123@gmail.com>
2023-06-19 09:42:38 +0530

    update ExpenseItem, create NewExpense

--- src/components/Expenses/ExpenseItem.jsx
+++ src/components/Expenses/ExpenseItem.jsx
@@ -1,15 +1,23 @@
+import React, { useState } from "react";
+
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 const ExpenseItem = ({ title, amount, date }) => {
+  const [newTitle, setNewTitle] = useState(title);
+
+  const clickHandler = () => {
+    setNewTitle("update!");
+  };
   return (
     <Card className="expense-item">
       <ExpenseDate date={date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{newTitle}</h2>
         <div className="expense-item__price">${amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 };
