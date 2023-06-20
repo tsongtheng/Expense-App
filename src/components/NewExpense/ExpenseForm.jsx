7cf06e9f56708005cb237d6db1ac0931b96efce1
Tipchan Songtheng <tipchansongtheng123@gmail.com>
2023-06-20 13:23:21 +0530

    update ExpenseForm (Store User input data after form submission).

--- src/components/NewExpense/ExpenseForm.jsx
+++ src/components/NewExpense/ExpenseForm.jsx
@@ -1,25 +1,97 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./ExpenseForm.css";
 const ExpenseForm = () => {
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredAmount, setEnteredAmount] = useState("");
+  const [enteredDate, setEnteredDate] = useState("");
+
+  // ALTERNATIVE TO MULTIPLE useState()
+  //   const [userInput, setUserInput] = useState({
+  //     enteredTitle: "",
+  //     enteredAmount: "",
+  //     enteredDate: "",
+  //   });
+
+  const titleChangeHandler = (event) => {
+    setEnteredTitle(event.target.value);
+
+    // ALTERNATIVE while using single useState()
+    // setUserInput((prevState) => {
+    //   return { ...prevState, enteredTitle: event.target.value };
+    // });
+  };
+
+  const amountChangeHandler = (event) => {
+    setEnteredAmount(event.target.value);
+  };
+
+  const dateChangeHandler = (event) => {
+    setEnteredDate(event.target.value);
+  };
+
+  // ALTERNATIVE TO MULTIPLE changeHandler
+  //   const inputChangeHandler = (identifier, value) => {
+  //     if (identifier === "title") {
+  //       setEnteredTitle(value);
+  //     } else if (identifier === "date") {
+  //       setEnteredDate(value);
+  //     } else {
+  //       setEnteredAmount(value);
+  //     }
+  //   };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const expenseData = {
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate),
+    };
+    console.log(expenseData);
+    setEnteredTitle("");
+    setEnteredAmount("");
+    setEnteredDate("");
+  };
+
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" />
+          <input
+            type="text"
+            value={enteredTitle}
+            onChange={titleChangeHandler}
+            // onChange={(event) =>inputChangeHandler("title", event.target.value)}
+          />
         </div>
       </div>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Amount</label>
-          <input type="number" min="0.01" step="0.01" />
+          <input
+            type="number"
+            min="0.01"
+            step="0.01"
+            value={enteredAmount}
+            onChange={amountChangeHandler}
+            // onChange={(event) => inputChangeHandler("amount", event.target.value)}
+          />
         </div>
       </div>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Date</label>
-          <input type="date" min="2019-01-01" max="2022-12-31" />
+          <input
+            type="date"
+            min="2019-01-01"
+            max="2022-12-31"
+            value={enteredDate}
+            onChange={dateChangeHandler}
+            // onChange={(event) => inputChangeHandler("date", event.target.value)}
+          />
         </div>
       </div>
 
