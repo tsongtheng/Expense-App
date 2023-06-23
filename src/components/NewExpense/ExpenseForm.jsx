7f5b219d2787124f71b94d13f84bb4e4861be009
Tipchan Songtheng <tipchansongtheng123@gmail.com>
2023-06-24 00:35:41 +0530

    bug fix in ExpenseForm component

--- src/components/NewExpense/ExpenseForm.jsx
+++ src/components/NewExpense/ExpenseForm.jsx
@@ -46,7 +46,7 @@ const ExpenseForm = ({ onSaveExpenseData, onCancel }) => {
 
     const expenseData = {
       title: enteredTitle,
-      amount: enteredAmount,
+      amount: +enteredAmount,
       date: new Date(enteredDate),
     };
     onSaveExpenseData(expenseData);
